8dec342c9ef9f3130215e89e16b115be29a6271f
Brad Gannon <bgannon@iastate.edu>
2019-04-10 19:16:12 -0500

    Added button

--- chess/src/App.js
+++ chess/src/App.js
@@ -3,8 +3,6 @@ import React, { Component } from "react";
 
 // Font-Awesome Library logos - usage here: https://fontawesome.com/how-to-use/on-the-web/using-with/react
 import { library } from "@fortawesome/fontawesome-svg-core";
-<<<<<<< HEAD
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faChessRook,
   faChessQueen,
@@ -15,11 +13,7 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 
 // Resources
-import logo from "./logo.svg";
 import "./App.css";
-import KingPiece from "./models/pieces/king-piece";
-import QueenPiece from "./models/pieces/queen-piece";
-import Board from "./components/board";
 import GameLogic from "./components/game-logic";
 
 library.add(
@@ -34,56 +28,37 @@ library.add(
 class App extends Component {
   constructor() {
     super();
-    this.king = new KingPiece("white");
-    this.queen = new QueenPiece("white");
-    this.blackKing = new KingPiece("black");
+    this.handleClick = this.handleClick.bind(this);
+    this.state = { clicked: false };
   }
+
+  handleClick() {
+    console.log("Clicked");
+    this.setState({ clicked: true });
+    this.renderBoard();
+  }
+
+  renderBoard() {
+    if (this.state.clicked) {
+      return (
+        <div className="Start">
+          <header className="Start-header">
+            <GameLogic />
+          </header>
+        </div>
+      );
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <GameLogic />
+          <button onClick={this.handleClick}>Button</button>
         </header>
       </div>
     );
   }
-=======
-import {
-	faChessRook,
-	faChessQueen,
-	faChessPawn,
-	faChessBishop,
-	faChessKnight,
-	faChessKing
-} from "@fortawesome/free-solid-svg-icons";
-
-// Resources
-import "./App.css";
-import GameLogic from "./components/game-logic";
-
-library.add(
-	faChessRook,
-	faChessQueen,
-	faChessPawn,
-	faChessBishop,
-	faChessKnight,
-	faChessKing
-);
-
-class App extends Component {
-	// constructor() {
-	// 	super();
-	// }
-	render() {
-		return (
-			<div className="App">
-				<header className="App-header">
-					<GameLogic />
-				</header>
-			</div>
-		);
-	}
->>>>>>> c4c0d841ece6b64537721ab12a77cf02bb973fe3
 }
 
 export default App;
