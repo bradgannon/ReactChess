9574f0fab91097f1943fb620b1b8d45c93fb74f1
Brad Gannon <bgannon@iastate.edu>
2019-04-11 13:33:54 -0500

    Styled and working button

--- chess/src/App.js
+++ chess/src/App.js
@@ -1,8 +1,10 @@
 // React Imports
 import React, { Component } from "react";
+import { Button } from "react-bootstrap";
 
 // Font-Awesome Library logos - usage here: https://fontawesome.com/how-to-use/on-the-web/using-with/react
 import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faChessRook,
   faChessQueen,
@@ -41,8 +43,8 @@ class App extends Component {
 
     if (isClicked) {
       return (
-        <div className="Start">
-          <header className="Start-header">
+        <div className="App">
+          <header className="App-header">
             <GameLogic />
           </header>
         </div>
@@ -50,8 +52,20 @@ class App extends Component {
     } else {
       return (
         <div className="App">
+          <link
+            rel="stylesheet"
+            href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css"
+          />
           <header className="App-header">
-            <button onClick={this.handleClick}>Button</button>
+            <p>
+              <button
+                href="#"
+                class="btn btn-success buttonCSS"
+                onClick={this.handleClick}
+              >
+                Start Game!
+              </button>
+            </p>
           </header>
         </div>
       );
