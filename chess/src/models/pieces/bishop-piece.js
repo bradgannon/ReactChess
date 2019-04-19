68ef922ce8e7c94f3662b65dd34a6cc86dbd910e
Zachary Mass <zmass@iastate.edu>
2019-04-19 14:52:34 -0500

    Bishop can now attack

--- chess/src/models/pieces/bishop-piece.js
+++ chess/src/models/pieces/bishop-piece.js
@@ -20,33 +20,89 @@ export default class BishopPiece extends ChessPiece {
 		// return an array of possible locations.
 		let validMoves = [];
 
-			// Check to the North East
-			let i = 1;
-			while (!b[location - 7 * i] && (location - 7 * i) % 8 !== 0 && location - 7 * i >= 0) {
+		// Check color of the piece moving
+		let enemy = "";
+		if (this.player === "white") {
+			enemy = "black";
+		} else {
+			enemy = "white";
+		}
+
+		// Check to the North East
+		let i = 1;
+		while (
+			!b[location - 7 * i] &&
+			(location - 7 * i) % 8 !== 0 &&
+			location - 7 * i >= 0
+		) {
+			validMoves.push(location - 7 * i);
+			i++;
+		}
+		if (
+			b[location - 7 * i] instanceof ChessPiece &&
+			(location - 7 * i) % 8 < 7
+		) {
+			if (b[location - 7 * i].player === enemy) {
 				validMoves.push(location - 7 * i);
-				i++;
 			}
-	
-			// Check to the North West
-			i = 1;
-			while (!b[location - 9 * i] && (location - 9 * i) % 8 !== 7 && location - 9 * i >= 0) {
+		}
+
+		// Check to the North West
+		i = 1;
+		while (
+			!b[location - 9 * i] &&
+			(location - 9 * i) % 8 !== 7 &&
+			location - 9 * i >= 0
+		) {
+			validMoves.push(location - 9 * i);
+			i++;
+		}
+		if (
+			b[location - 9 * i] instanceof ChessPiece &&
+			(location - 9 * i) % 8 < 7
+		) {
+			if (b[location - 9 * i].player === enemy) {
 				validMoves.push(location - 9 * i);
-				i++;
 			}
-	
-			// Check to the South East
-			i = 1;
-			while (!b[location + 9 * i] && (location + 9 * i) % 8 !== 0 && location + 9 * i < 64) {
+		}
+
+		// Check to the South East
+		i = 1;
+		while (
+			!b[location + 9 * i] &&
+			(location + 9 * i) % 8 !== 0 &&
+			location + 9 * i < 64
+		) {
+			validMoves.push(location + 9 * i);
+			i++;
+		}
+		if (
+			b[location + 9 * i] instanceof ChessPiece &&
+			(location + 9 * i) % 8 < 7
+		) {
+			if (b[location + 9 * i].player === enemy) {
 				validMoves.push(location + 9 * i);
-				i++;
 			}
-	
-			// Check to the South West
-			i = 1;
-			while (!b[location + 7 * i] && (location + 7 * i) % 8 !== 7 && location + 7 * i < 64) {
+		}
+
+		// Check to the South West
+		i = 1;
+		while (
+			!b[location + 7 * i] &&
+			(location + 7 * i) % 8 !== 7 &&
+			location + 7 * i < 64
+		) {
+			validMoves.push(location + 7 * i);
+			i++;
+		}
+		if (
+			b[location + 7 * i] instanceof ChessPiece &&
+			(location + 7 * i) % 8 < 7
+		) {
+			if (b[location + 7 * i].player === enemy) {
 				validMoves.push(location + 7 * i);
-				i++;
 			}
+		}
 
 		return validMoves;
 	}
