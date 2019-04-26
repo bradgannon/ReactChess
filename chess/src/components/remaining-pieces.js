dfbf587ad329d0cc34cbad1892d1aac112da54f1
Shubham <schauhan@iastate.edu>
2019-04-26 00:54:49 -0500

    Adding functionality to remaining pieces Queen pieces

--- chess/src/components/remaining-pieces.js
+++ chess/src/components/remaining-pieces.js
@@ -190,7 +190,41 @@ class RemainingPieces extends Component {
 			return (this.renderBishop(piece, key));
 		} 
 
-		// Render Queen
+		//Rendr Queen
+
+		renderQueen(piece, key) {
+		console.log("Shub implement this");
+		if (piece.player === "black") {
+			// Note: The Style property is CSS code - this allows an outline to be shown on the piece
+			return (
+				<FontAwesomeIcon
+					icon="chess-Queen"
+					color={piece.player}
+					size="1x"
+					key={key}
+					style={{ stroke: "white", strokeWidth: 15 }}
+				/>
+			);
+		} else if (piece.player === "white") {
+			// Note: The Style property is CSS code - this allows an outline to be shown on the piece
+			return (
+				<FontAwesomeIcon
+					icon="chess-Queen"
+					color={piece.player}
+					size="1x"
+					key={key}
+					style={{ stroke: "black", strokeWidth: 15 }}
+				/>
+			)
+		}
+	}
+	renderPiece(piece, key) {
+		// console.log(piece);
+		if (piece.typeOfPiece === "pawn") {
+			return (this.renderPawn(piece, key));
+		} else if (piece.typeOfPiece === "Queen") {
+			return (this.renderQueen(piece, key));
+		}
 
 		// Render King
 		else {
