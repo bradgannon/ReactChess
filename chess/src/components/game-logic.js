c08abab22f599f15183158bdaedeb276651d0f1a
Brad Gannon <bgannon@iastate.edu>
2019-04-29 12:12:59 -0500

    Weird merge issues

--- chess/src/components/game-logic.js
+++ chess/src/components/game-logic.js
@@ -4,7 +4,7 @@ import Board from "./board";
 import RemainingPieces from "./remaining-pieces";
 
 import { updateBoard } from "../redux/action/index";
-import { Grid } from '@material-ui/core/';
+import { Grid } from "@material-ui/core/";
 
 /**
  * Contains the logic and hosts all of the core chess components
@@ -31,63 +31,35 @@ class GameLogic extends Component {
       playerTurn = this.props.player2Name + "'s Turn";
     }
     return (
-      <div className="game-logic">
-        <div className="board-container">
-          <Board
-            blocks={this.props.board}
-            onClick={i => {
-              this.handleClick(i);
-            }}
-          />
-        </div>
-        <div>
-          {/* insert Current Player's Move status. */}
-          <div className="playerTurn">
-            <p>{playerTurn}</p>
-          </div>
-        </div>
-        {/* {Insert component of lost pieces for each player here} */}
+      <div>
+        <Grid container direction="row" justify="center" alignItems="center">
+          <Grid item xs={2} alignItems="center" justify="center">
+            <RemainingPieces />
+          </Grid>
+          <Grid
+            item
+            xs={10}
+            alignContent={"center"}
+            style={{ maxWidth: 900, minWidth: 900 }}
+          >
+            <Board
+              blocks={this.props.board}
+              onClick={i => {
+                this.handleClick(i);
+              }}
+            />
+          </Grid>
+        </Grid>
+        <Grid container>
+          <Grid item xs={12}>
+            <div className="playerTurn">
+              <p>{playerTurn}</p>
+            </div>
+          </Grid>
+        </Grid>
       </div>
     );
   }
-
-	render() {
-		let playerTurn = "";
-    if (this.props.playerTurn === "white") {
-      playerTurn = this.props.player1Name + "'s Turn";
-    } else {
-      playerTurn = this.props.player2Name + "'s Turn";
-    }
-		return (
-			<div>
-			<Grid container direction="row" justify="center" alignItems="center" >
-				<Grid item xs={2} alignItems="center" justify="center">
-					<RemainingPieces />
-				</Grid>
-				<Grid item xs={10} alignContent={"center"} style={{maxWidth: 900, minWidth: 900}} >
-					<Board
-						blocks={this.props.board}
-						onClick={i => {
-							this.handleClick(i);
-						}} />
-				</Grid>
-			</Grid>
-			<Grid container>
-				<Grid item xs={12} >
-				<div className="playerTurn">
-						<p>{playerTurn}</p>
-					</div>
-				</Grid>
-			</Grid>
-			</div>
-		
-
-		);
-	}
-<<<<<<< HEAD
->>>>>>> 6948e14173a331b40149b1e58d1596829ef8dfcb
-=======
->>>>>>> d0f53d871a96f1428e91d8c9f46346eb934013a2
 }
 
 function mapStateToProps(state) {
